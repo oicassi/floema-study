268fe715168f8274d3b9a6fbc7cae9ee22ec17aa
Cassiano Kruchelski Vidal <casst@protonmail.com>
2022-03-30 07:41:52 -0300

    add partials and basic template structure

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Floema website'
+      }
+    }
+  })
 })
 
 app.listen(port, () => {
