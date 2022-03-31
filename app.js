7416df5f63971b67cd1b93667c56badcb20f4369
Cassiano Kruchelski Vidal <casst@protonmail.com>
2022-03-31 09:49:44 -0300

    add pages

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    meta: {
-      data: {
-        title: 'Floema',
-        description: 'Floema website'
-      }
-    }
-  })
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
 })
 
 app.listen(port, () => {
