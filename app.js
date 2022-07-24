3d122ef0e533e0daef58c062297c8417c142c8a1
Cassiano Kruchelski <casst@protonmail.com>
2022-07-23 22:39:37 -0300

    adjust navigation

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import methodOverride from 'method-override'
+import logger from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import * as prismicHelpers from '@prismicio/helpers'
@@ -18,23 +21,43 @@ app.use((req, res, next) => {
   res.locals.ctx = {
     prismicHelpers,
   }
+  res.locals.indexToText = indexToText
+  res.locals.Link = handleLinkResolver
   next()
 })
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride())
+
+app.get('/', async (req, res) => {
+  try {
+    const defaultData = await getDefaultData()
+    const promises = [prismicClient.getSingle('home'), prismicClient.getAllByType('collection')]
+    const [home, collections] = await Promise.all(promises)
+
+    res.render('pages/home', {
+      ...defaultData,
+      home,
+      collections,
+    })
+  } catch (error) {
+    console.log('error fetching data for home page', error)
+  }
 })
 
 app.get('/about', async (req, res) => {
   try {
+    const defaultData = await getDefaultData()
     const {
-      results: [meta, about],
+      results: [about],
     } = await prismicClient.get({
-      predicates: prismic.predicate.any('document.type', ['meta', 'about']),
+      predicates: prismic.predicate.any('document.type', ['about']),
     })
 
     res.render('pages/about', {
-      meta,
+      ...defaultData,
       about,
     })
   } catch (error) {
@@ -46,14 +69,16 @@ app.get('/collections', async (req, res) => {
   // Example using predicates (using predicate it's possible to have pagination)
   // prismicClient.get({ predicates: prismic.predicate.at('document.type', 'collection') }),
   try {
+    const defaultData = await getDefaultData()
+    console.log('defaultData')
+    console.log(defaultData)
     const promises = [
-      prismicClient.getSingle('meta'),
       prismicClient.getSingle('home'),
       prismicClient.getAllByType('collection', { fetchLinks: ['product.image'] }),
     ]
-    const [meta, home, collections] = await Promise.all(promises)
+    const [home, collections] = await Promise.all(promises)
     res.render('pages/collections', {
-      meta,
+      ...defaultData,
       home,
       collections,
     })
@@ -66,14 +91,10 @@ app.get('/detail/:uid', async (req, res) => {
   const uid = req.params.uid
 
   try {
-    const promises = [
-      prismicClient.getSingle('meta'),
-      prismicClient.getByUID('product', uid, { fetchLinks: 'collection.title' }),
-    ]
-    const [meta, product] = await Promise.all(promises)
-
+    const defaultData = await getDefaultData()
+    const product = await prismicClient.getByUID('product', uid, { fetchLinks: 'collection.full_title' })
     res.render('pages/detail', {
-      meta,
+      ...defaultData,
       product,
     })
   } catch (error) {
@@ -84,3 +105,28 @@ app.get('/detail/:uid', async (req, res) => {
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`)
 })
+
+// Helpers
+const getDefaultData = async () => {
+  const promises = [
+    prismicClient.getSingle('meta'),
+    prismicClient.getSingle('preloader'),
+    prismicClient.getSingle('navigation'),
+  ]
+
+  const [meta, preloader, navigation] = await Promise.all(promises)
+  return { meta, preloader, navigation }
+}
+
+const indexToText = (index) => {
+  const textIndexes = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+  return textIndexes[index]
+}
+
+const handleLinkResolver = (doc) => {
+  if (doc.type === 'product') return `/detail/${doc.uid}`
+  if (doc.type === 'about') return '/about'
+  if (doc.type === 'collections') return '/collections'
+
+  return '/'
+}
